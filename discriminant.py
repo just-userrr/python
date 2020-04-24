6a467ae97e9d966097edb54606fb12eea5998764
Just_User <62239076+just-userrr@users.noreply.github.com>
2020-04-24 23:01:14 +0600

    Update discriminant.py
    Работает криво, зато красиво! Если че, ответы неправильные, базарю.

--- discriminant.py
+++ discriminant.py
@@ -7,11 +7,16 @@ Version: 2.0
 
 This programm counts discriminant
 """
+import os
 from termcolor import colored
 from colorama import Fore, Back, Style
 import math
 
 print("") # для отступа
+
+# автоматическая установка всех необходимых модулей
+os.system('pip3 install termcolor')
+os.system('pip3 install colorama')
 
 # входные данные
 a = int(input( Back.YELLOW + Fore.BLACK + "Введите а: " ))
