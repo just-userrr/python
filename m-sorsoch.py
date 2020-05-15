914ee1b6e692cbec5f6c35a1dd632ae48459cd93
Just_User <62239076+just-userrr@users.noreply.github.com>
2020-05-15 11:18:30 +0600

    Add files via upload

---
+++ m-sorsoch.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Result of sor/soch
+
+Version: 2.2.2
+
+Created by Erasyl_Seralyuly
+"""
+from colorama import Fore, Back, Style, init
+init()
+
+print( "\033[1m\033[37m" )
+
+# максимальный балл
+mball = int(input( "Максимальный балл: " ))
+
+# баллы, которую я получил
+tball = int(input( "Сколько ты получил баллов: " ))
+print( "" )
+
+# вычисление
+result = (tball * 100) / mball
+
+# выходные данные
+result_text = "Ты получил " + str( int(result) ) + " процентов"
+# итоговые оценки
+if result >= 85 and result <= 100:
+	print( "\033[2m\033[32m{}".format( result_text ) )
+	print( "\033[2m\033[32m{}".format( "Оценка: 5" ) )
+elif result > 64 and result <= 84:
+	print( "\033[1m\033[32m{}".format( result_text ) )
+	print( "Оценка: 4" )
+elif result > 40 and result <= 64:
+	print( "\033[1m\033[33m{}".format( result_text ) )
+	print( "Оценка: 3" )
+elif result >= 1 and result <= 39:
+	print( "\033[1m\033[31m{}".format( result_text ) )
+	print( "Оценка: 2" )
+
+# на случай, если данные были введены неверно
+if result > 100:
+	print( "\033[1m\033[31m{}".format("Значение написано неправильно!") )
+elif result < 0:
+	print( "\033[1m\033[31m{}".format("Значение написано неправильно!") )
+elif result == 0:
+	print( "\033[1m\033[31m{}".format("Значение написано неправильно!") )
+
+print( Style.RESET_ALL + Fore.RESET )
