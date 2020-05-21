51698fb1f75e6ae971b277e8199053c2776b2912
Just_User <62239076+just-userrr@users.noreply.github.com>
2020-05-21 20:31:20 +0600

    Update install_modules.py

--- install_modules.py
+++ install_modules.py
@@ -5,10 +5,5 @@
 """
 import os # модуль для управления с команадами в терминале
 
-# прописываем команду в терминале для установки модуля termcolor для каждой из версий python
-# установка модуля termcolor
-os.system( 'pip install termcolor' ) # установка модуля для Python с версие 2.x
-os.system( 'pip3 install termcolor' ) # установка модуля для Python с версией 3.x
-# установка модуля colorama
-os.system( 'pip install colorama' ) # установка модуля для Python с версией 2.x
-os.system( 'pip3 install colorama' ) # установка модуля для Python с версией 3.x
+os.system( 'pip install termcolor' ) # установка модуля termcolor
+os.system( 'pip install colorama' ) # установка модуля colorama
